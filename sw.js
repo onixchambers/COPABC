a647abe02a64c392fa8a9c43bc32c543f479e85d
Bryan <bryan@antigravity.local>
2026-09-03 21:17:16 -0500

    Agregar logo AERO_BASS.png, manifest.json, sw.js y configuracion Netlify

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'aerobass-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './BC23.0BASS.html',
+  './AERO_BASS.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).catch(() => caches.match('./index.html'));
+      })
+  );
+});
